0634a666e856bf9eaef03bd0a48756a55ce9b739
thomas-cormican <thomascormican123@gmail.com>
2024-04-27 19:45:02 +0100

    fix email form

--- utils/mailer.js
+++ utils/mailer.js
@@ -14,8 +14,8 @@ export default async (body) => {
 
     transporter.sendMail(
       {
-        from: `${body.name} <info@oxycholy.com>`, // sender address
-        to: "info@oxycholy.com", // list of receivers
+        from: `${body.name} <${process.env.EMAIL_USER}>`, // sender address
+        to: `${process.env.EMAIL_USER}`, // list of receivers
         subject: "Oxycholy Form", // Subject line
         text:
           body.message +
@@ -27,27 +27,25 @@ export default async (body) => {
           reject(err);
           return "Error";
         } else {
-          resolve(info);
-          console.log("sent");
-          return "Ok";
-        }
-      }
-    );
-
-    transporter.sendMail(
-      {
-        from: `Oxycholy <info@oxycholy.com>`, // sender address
-        to: body.email, // list of receivers
-        subject: "Confirmation: Your Email to Oxycholy Has Been Received", // Subject line
-        text: `Dear ${body.name},\n\nThank you for reaching out to Oxycholy.\nThis is an automated confirmation to let you know that we've received your email. We appreciate you taking the time to contact us.\nOur team is reviewing your message and will get back to you as soon as possible. Rest assured, we're committed to addressing your concerns or inquiries promptly.`, // plain text body
-      },
-      (err, info) => {
-        if (err) {
-          console.error(err);
-          reject(err);
-          return "Error";
-        } else {
-          resolve(info);
+          transporter.sendMail(
+            {
+              from: `Oxycholy <${process.env.EMAIL_USER}>`, // sender address
+              to: body.email, // list of receivers
+              subject: "Confirmation: Your Email to Oxycholy Has Been Received", // Subject line
+              text: `Dear ${body.name},\n\nThank you for reaching out to Oxycholy.\nThis is an automated confirmation to let you know that we've received your email. We appreciate you taking the time to contact us.\nOur team is reviewing your message and will get back to you as soon as possible.Rest assured, we're committed to addressing your concerns or inquiries promptly.`, // plain text body
+            },
+            (err, info) => {
+              if (err) {
+                console.error(err);
+                reject(err);
+                return "Error";
+              } else {
+                resolve(info);
+                console.log("sent");
+                return "Ok";
+              }
+            }
+          );
           console.log("sent");
           return "Ok";
         }
